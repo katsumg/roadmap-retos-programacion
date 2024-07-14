84f1d8afc418f68e5b07957411e60b291484b024
katsumg <michxlgaa@gmail.com>
2024-07-14 23:37:39 +0100

    Create katsu.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/katsu.py
@@ -0,0 +1,18 @@
+
+#comentario en una linea
+
+my_variable = "Mi variable" #forma de representar un variable
+my_variable = "Mi variable"
+
+MY_CONSTANT  = "Mi constante" #esta tratando de decir que esto es una constancia
+#-------------------------------------------------
+#Tipos de datos primitivos 
+my_int = 1 #para nùmeros enteros se usa int
+my_float = 1.3 #para nùmeros decimales
+my_bool = True  #para valores boleanos como true o false
+my_bool = False 
+my_string = "Mi cadena de texto"
+my_other_string = "Mi otra cadena de texto"
+
+print("Hola mi nombre es michael")  #para imprimir texto susamos print con parentesis y comillas 
+    
